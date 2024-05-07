e87c3821419c7323288b1f08ef9ad6592926549c
numerous <ivankin1995rus@gmail.com>
2024-05-07 10:11:28 +0200

    [sd-03] sql

--- ru/flowers-shop/05-lesson/init.sql
+++ ru/flowers-shop/05-lesson/init.sql
@@ -16,29 +16,29 @@ CREATE TABLE "orders" (
 -- if we need to create partitions and we already have the table - we have to create a new one.
 -- we should use user_uuid as UUID type
 CREATE TABLE IF NOT EXISTS "orders_1" (
-                                          id SERIAL,
-                                          address CHAR(255),
-                                          description VARCHAR(255),
-                                          receiver_name VARCHAR(255),
-                                          receive_time TIMESTAMP,
-                                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                          updated_at TIMESTAMP,
-                                          offer_id INT NOT NULL,
-                                          status CHAR(15),
-                                          user_uuid UUID NOT NULL PRIMARY KEY, -- important
-                                          customer_id INT NOT NULL
+                              id SERIAL,
+                              address CHAR(255),
+                              description VARCHAR(255),
+                              receiver_name VARCHAR(255),
+                              receive_time TIMESTAMP,
+                              created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                              updated_at TIMESTAMP,
+                              offer_id INT NOT NULL,
+                              status CHAR(15),
+                              user_uuid UUID NOT NULL PRIMARY KEY, -- important
+                              customer_id INT NOT NULL
 ) PARTITION BY HASH (user_uuid);
 -- or
 CREATE TABLE IF NOT EXISTS "orders_2" (
-                                          id SERIAL,
-                                          address CHAR(255),
-                                          description VARCHAR(255),
-                                          receiver_name VARCHAR(255),
-                                          receive_time TIMESTAMP,
-                                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                          updated_at TIMESTAMP,
-                                          offer_id INT NOT NULL,
-                                          status CHAR(15),
-                                          user_uuid UUID NOT NULL, -- important
-                                          customer_id INT NOT NULL
+                              id SERIAL,
+                              address CHAR(255),
+                              description VARCHAR(255),
+                              receiver_name VARCHAR(255),
+                              receive_time TIMESTAMP,
+                              created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                              updated_at TIMESTAMP,
+                              offer_id INT NOT NULL,
+                              status CHAR(15),
+                              user_uuid UUID NOT NULL, -- important
+                              customer_id INT NOT NULL
 ) PARTITION BY HASH (user_uuid);
